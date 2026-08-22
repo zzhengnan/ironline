2a9b1e9337b7b3de477162aa050b7984c02ae913
Zhengnan Zhao <17503044+zzhengnan@users.noreply.github.com>
2026-08-22 12:58:10 -0400

    Add test

--- README.md
+++ README.md
@@ -12,9 +12,13 @@ conda activate ironline
 # Install Python wrapper into current env (via pip)
 maturin develop
 
+# Invoke from Python wrapper
 >>> import ironline
 >>> ironline
 <module 'ironline' from '~/miniforge3/envs/ironline/lib/python3.14/site-packages/ironline/__init__.py'>
 >>> ironline.compute_mean([1, 3, 4])
 2.6666666666666665
+
+# Run tests
+DYLD_LIBRARY_PATH="$CONDA_PREFIX/lib" cargo test
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,22 @@ mod ironline {
     use pyo3::prelude::*;
 
     #[pyfunction]
-    fn compute_mean(numbers: Vec<f64>) -> f64 {
+    pub fn compute_mean(numbers: Vec<f64>) -> f64 {
         let mut sum = 0.0;
         for number in &numbers {
             sum += number;
         }
         let len = numbers.len() as f64;
         sum / len
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test1() {
+        let result = ironline::compute_mean(vec![1.0, 2.0, 3.0]);
+        assert_eq!(result, 2.0);
     }
 }
